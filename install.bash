dd2967f62524b5d18cd1f89db3e55fdee3da6652
xyavinash <avinashyadav.psu@gmail.com>
2021-05-23 17:12:49 -0400

    Jen_Ansi_git

---
+++ install.bash
@@ -0,0 +1,21 @@
+udo yum install java-1.8.0-openjdk-devel 
+
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repotex
+text editor quit
+
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+sudo yum install jenkins 
+
+
+
+sudo yum install epel-release 
+
+sudo yum install ansible 
+
+sudo vi /etc/ansible/hosts
+
+yum install git 
+
